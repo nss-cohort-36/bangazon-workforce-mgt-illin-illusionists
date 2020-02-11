05d2b1f70dcf38f09a06db42a8d5719c81821e58
Melody Stern <melliemuse@gmail.com>
2020-02-11 14:40:40 -0600

    updated computer model & fixtures

--- hrapp/models/computer.py
+++ hrapp/models/computer.py
@@ -11,7 +11,8 @@ class Computer(models.Model):
       employees: This property contains the many to many relationship with the computer/employee model
     '''
 
-    make = models.CharField(max_length=20)
+    manufacturer = models.CharField(max_length=25)
+    model = models.CharField(max_length=25)
     purchase_date = models.DateField()
     decommission_date = models.DateField(null=True, blank=True, default=None)
     employees = models.ManyToManyField("Employee", through='EmployeeComputer')
