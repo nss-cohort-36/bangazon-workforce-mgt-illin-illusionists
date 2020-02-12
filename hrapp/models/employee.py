550614c45a8707d38db3e32cce8d06b9c373c3b3
Jesie Oldenburg <kitglo46@gmail.com>
2020-02-12 14:59:50 -0600

    added foreign key requirement and imported model for testing the assigned departments for an employee

--- hrapp/models/employee.py
+++ hrapp/models/employee.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from .department import Department
+from .department import Department
 
 class Employee(models.Model):
 
@@ -7,7 +7,7 @@ class Employee(models.Model):
     last_name = models.CharField(max_length=100)
     start_date = models.DateField()
     is_supervisor = models.BooleanField()
-    # department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     class Meta:
         verbose_name = ("Employee")
