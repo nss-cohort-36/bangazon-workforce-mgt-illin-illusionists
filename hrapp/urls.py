a09bd16b61803f8ba821f0f58a57d803c924ab1f
Sophia Hoffman <sophiahoffman@hotmail.com>
2020-02-13 10:09:08 -0600

    stage to approve pr

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -12,6 +12,6 @@ urlpatterns = [
     path('departments/', employee_list, name='employee_list'),
     path('computers/', computer_list, name='computer_list'),
     path('trainingprograms/', training_program_list, name='training_list'),
-    path('computers/<int:computer_id>', computer_details, name="computer")
-
+    path('computers/<int:computer_id>', computer_details, name="computer"),
+    path('computer/form', computer_form, name = "computer_form")
 ]
