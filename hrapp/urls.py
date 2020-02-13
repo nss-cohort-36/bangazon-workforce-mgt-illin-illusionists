4244ec86728862487461c571e964ff14d314c2b6
Jesie Oldenburg <kitglo46@gmail.com>
2020-02-13 15:48:12 -0600

    individual details now printing for departments

--- hrapp/urls.py
+++ hrapp/urls.py
@@ -9,13 +9,14 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', logout_user, name='logout'),
     path('employees/', employee_list, name='employee_list'),
-    path('departments/', department_list, name='department_list'), 
-    path('departments/<int:department_id>/', department_details, name='department_details'), 
+    
     path('computers/', computer_list, name='computer_list'),
     path('trainingprograms/', training_program_list, name='training_list'),
     path('trainingprograms/form', training_program_form, name='training_form'),
     path('computers/<int:computer_id>', computer_details, name="computer"),
     path('computer/form', computer_form, name = "computer_form"),
-    path('employees/<int:employee_id>', employee_details, name="employee")
-
+    path('employees/<int:employee_id>', employee_details, name="employee"),
+    
+    path('departments/', department_list, name='department_list'), 
+    path('departments/<int:department_id>/', department_details, name='department')
 ]
--- hrapp/views/departments/department_details.py
+++ hrapp/views/departments/department_details.py
@@ -1,5 +1,5 @@
 import sqlite3
-from django.urls import reverse
+# from django.urls import reverse
 from django.shortcuts import render, redirect
 # from django.contrib.auth.decorators import login_required
 from hrapp.models import Department
@@ -11,28 +11,23 @@ def get_department(department_id):
         conn.row_factory = model_factory(Department)
         db_cursor = conn.cursor()
 
-#         SELECT 
-# e.first_name,
-# e.last_name,
-# d.dept_name
-# FROM hrapp_employee e, hrapp_department d
-# WHERE e.department_id = d.id;
-
         db_cursor.execute("""
         SELECT 
             e.first_name,
             e.last_name,
-            d.dept_name
+            d.dept_name,
+            d.budget
             FROM hrapp_employee e, hrapp_department d
-            where e.department_id = d.id;
+            where d.id = ?;
         """, (department_id,))
 
-        return db_cursor.fetchone()
+        department = db_cursor.fetchone()
+        return department
 
 # @login_required
 def department_details(request, department_id):
     if request.method == 'GET':
-        library = get_department(department_id)
+        department = get_department(department_id)
 
         template = 'departments/department_details.html'
         context = {
