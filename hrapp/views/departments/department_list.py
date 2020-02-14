555eb4e76b5bb58228611c16407ffbe4ebff6336
Jesie Oldenburg <kitglo46@gmail.com>
2020-02-14 12:14:23 -0600

    department form is now rendering and adding depts to the db

--- hrapp/views/departments/department_list.py
+++ hrapp/views/departments/department_list.py
@@ -1,5 +1,6 @@
 import sqlite3
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from hrapp.models import Department
 from hrapp.models import model_factory
 from ..connection import Connection
@@ -19,9 +20,23 @@ def department_list(request):
 
             all_departments = db_cursor.fetchall()
 
-    template = 'departments/department_list.html'
-    context = {
-        'departments': all_departments
-    }
+            template = 'departments/department_list.html'
+            context = {
+                'departments': all_departments
+            }
 
-    return render(request, template, context)
+            return render(request, template, context)
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        with sqlite3.connect(Connection.db_path) as conn:
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            INSERT INTO hrapp_department
+            (dept_name, budget)
+            VALUES (?, ?)
+            """, (form_data['dept_name'], form_data['budget']))
+
+        return redirect(reverse('hrapp:department_list'))
